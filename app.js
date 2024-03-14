55d12ed7c900928e4acdf4e04bfcef71df06a477
Saianlord <iansacr12@gmail.com>
2024-03-14 00:36:30 -0600

    actualización en backend. Se añade función de agregar y remover servicios

--- app.js
+++ app.js
@@ -1,1 +1,37 @@
-console.log("Ejecutando el sistema")
+function cotizar(){
+    window.location.href = 'contacto.html';
+
+}
+var serviciosEscogidos = [];
+
+
+function agregarServicio(servicio, boton){
+    
+
+    serviciosEscogidos.push(servicio)
+
+    console.log(serviciosEscogidos);
+
+    boton.style.display = 'none';
+
+    boton.nextElementSibling.style.display = 'block';
+
+}
+
+function removerServicio(servicio, boton){
+
+    var indice = serviciosEscogidos.indexOf(servicio);
+    
+    if (indice !== -1) {
+    serviciosEscogidos.splice(indice, 1);
+    }
+
+    console.log(serviciosEscogidos);
+
+
+    boton.style.display = 'none';
+
+    boton.previousElementSibling.style.display = 'block';
+
+    
+}
